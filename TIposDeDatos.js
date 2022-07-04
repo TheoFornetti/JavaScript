8e83f2d0aaa32dc05bb7e5d7313a81dad32dc3b5
Theo Fornetti <theoforentti2001@gmail.com>
2022-07-04 11:47:31 -0300

    Tipos de datos

---
+++ TIposDeDatos.js
@@ -0,0 +1,36 @@
+//Nos permiten enetender caracteristicas de cada tipo de dato
+//Tipos de datos
+
+//Number: dato numerico 
+let edad = 32;
+let paga = 10.000;
+
+//Strings: cadena de texto 
+let nombreCompleto = "Theo Fornetti"
+
+//boolean: true o false
+let estaVacio = false;
+let esArgentino = true;
+
+//Nan: sirva para especificar cuando no es un numero en si. POr ejempo a+b=nAn
+let notANumber = NaN
+
+//null: Sirva para asignar una valor vacio a alguna variable la cual deaconocemos su valor 
+let capacidadEdificio=null;
+
+//undefined: cuando no tiene un valor definido
+let nulo;
+
+//Objetos: 
+
+let MiComputadora = {
+    marca : 'Asus',
+    modelo : 'VivoBook',
+    anio : 2022
+}
+
+//Arrays: 
+let peliculas = [
+    'transformers', 'Batman', 'Civil War', 'SpiderMan 3'
+]
+
